fb298953d8a56bb8ea4f8b7587acb4601c506d25
GameDevKnight <seepsoda@gmail.com>
2023-07-25 10:48:06 +1000

    EpisodeEleven

--- Scripts/Enemies/Sabertooth.gd
+++ Scripts/Enemies/Sabertooth.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var speed = -60.0
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -8,8 +7,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var facing_right = false
 var dead = false
 
+var max_health = 1
+var health
+
 
 func _ready():
+	health = max_health
 	$AnimationPlayer.play("Run")
 
 func _physics_process(delta):
@@ -34,6 +37,12 @@ func flip():
 func _on_hitbox_area_entered(area):
 	if area.get_parent() is Player && !dead:
 		area.get_parent().take_damage(1)
+
+func take_damage(damage_amount):
+	health -= damage_amount
+	
+	if health <= 0:
+		die()
 
 func die():
 	dead = true
