09c242341999fbe48a5227d26693017577174d79
doggodreams <bradbury13@live.com.au>
2024-08-19 18:46:10 +1000

    Changed the way jumping works.

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -4,17 +4,27 @@ class_name Player
 @onready var animation = $AnimationPlayer
 @onready var sprite = $Sprite2D
 
-@export var speed = 300.0
-@export var jump_velocity = -400.0
 
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var move_speed = 200.0  # New movement speed for horizontal movement
 
 @export var attacking = false
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var max_health = 2
 var health = 0
 var can_take_damage = true
 @export var hit = false
+
+var movement_velocity := Vector2.ZERO
+
+@export var jump_height : float = 300.0
+@export var jump_time_to_peak : float = 0.5
+@export var jump_time_to_descent : float = 0.5
+
+@onready var jump_velocity : float = ((2.0 * jump_height) / jump_time_to_peak) * -1.0
+@onready var jump_gravity : float = ((-2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * -1.0
+@onready var fall_gravity : float = ((-2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * -1.0
 
 func _ready():
 	GameManager.damage_taken = 0
@@ -26,33 +36,38 @@ func _process(_delta):
 		attack()
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("left"):
-		sprite.scale.x = abs(sprite.scale.x) * -1
-		$AttackArea.scale.x = abs($AttackArea.scale.x) * -1
-	if Input.is_action_just_pressed("right"):
-		sprite.scale.x = abs(sprite.scale.x)
-		$AttackArea.scale.x = abs($AttackArea.scale.x)
-	
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	velocity.y += get_gravity() * delta
+	velocity.x = get_input_velocity() * move_speed
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		jump()
 	
-	if Input.is_action_pressed("down") and is_on_floor():
-		position.y += 5
-	
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	move_and_slide()
 	
 	update_animation()
-	move_and_slide()
 	
 	if position.y >= 700:
 		die()
+
+func get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func jump():
+	velocity.y = jump_velocity
+
+func get_input_velocity() -> float:
+	var horizontal := 0.0
+	
+	if Input.is_action_pressed("left"):
+		horizontal -= 1.0
+		sprite.scale.x = abs(sprite.scale.x) * -1
+		$AttackArea.scale.x = abs($AttackArea.scale.x) * -1
+	if Input.is_action_pressed("right"):
+		horizontal += 1.0
+		sprite.scale.x = abs(sprite.scale.x)
+		$AttackArea.scale.x = abs($AttackArea.scale.x)
+	
+	return horizontal
 
 func attack():
 	var overlapping_objects = $AttackArea.get_overlapping_areas()
@@ -103,4 +118,3 @@ func die():
 	animation.play("Die")
 	await animation.animation_finished
 	GameManager.respawn_player()
-
